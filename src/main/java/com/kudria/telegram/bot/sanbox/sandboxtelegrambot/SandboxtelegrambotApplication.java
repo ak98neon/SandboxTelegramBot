ab4b2dc9b91e08dd069cc0aa67243e2390099123
ak98neon <artem_kudria@epam.com>
2019-09-10 21:51:17 +0300

    first commit

---
+++ src/main/java/com/kudria/telegram/bot/sanbox/sandboxtelegrambot/SandboxtelegrambotApplication.java
@@ -0,0 +1,13 @@
+package com.kudria.telegram.bot.sanbox.sandboxtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxtelegrambotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxtelegrambotApplication.class, args);
+    }
+
+}
